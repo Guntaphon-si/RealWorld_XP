6322f86ba720429a7fae79bab33a2a0349913e27
KittiphatDuangmanee <phat6452@gmail.com>
2025-10-05 20:13:29 +0700

    buttonRename

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { AssessMentQuestion } from './assess-ment-question/assess-ment-question';
 import { ResultAndChooseActivity } from './result-and-choose-activity/result-and-choose-activity';
-import { ButtonTest } from './button-test/button-test';
+import { ButtonTest } from './button-test1/button-test';
 export const routes: Routes = [
   {
     path: 'assessment', // เมื่อเข้ามาที่ path หลัก (เช่น yoursite.com)
